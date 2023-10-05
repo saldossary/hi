7def048e30f2853b8cfcc1aa0694d07cb01f4c0c
saldossary <sheikha.aldossary.apple@gmail.com>
2023-10-05 12:07:34 +0300

    Initial Commit

---
+++ hi/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  hi
+//
+//  Created by sheikha aldossary on 20/03/1445 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ hi/hiApp.swift
@@ -0,0 +1,17 @@
+//
+//  hiApp.swift
+//  hi
+//
+//  Created by sheikha aldossary on 20/03/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct hiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
